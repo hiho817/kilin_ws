745bfb12f19fa0e70f19523f5087b500a082870d
ian920201 <ian20030201@gmail.com>
2026-04-10 11:57:20 +0800

    fix: add missing parameter to buildLegCmd calls for modules in KilinGaitPlayer

--- kilin_ros_ws/src/kilin_csv_control/src/kilin_csv_control.cpp
+++ kilin_ros_ws/src/kilin_csv_control/src/kilin_csv_control.cpp
@@ -349,13 +349,13 @@ private:
                                    p0.a_hip_pos, p1.a_hip_pos,
                                    p0.a_steer_pos, p1.a_steer_pos,
                                    p1.a_hub_vel,
-                                   p1.a_hub_mode);
+                                   p1.a_hub_mode, true);
 
         msg.module_b = buildLegCmd(t, p0, p1,
                                    p0.b_hip_pos, p1.b_hip_pos,
                                    p0.b_steer_pos, p1.b_steer_pos,
                                    p1.b_hub_vel,
-                                   p1.b_hub_mode);
+                                   p1.b_hub_mode, true);
 
         msg.module_c = buildLegCmd(t, p0, p1,
                                    p0.c_hip_pos, p1.c_hip_pos,
